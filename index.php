a59aa83429790388d989951ef114a510f6d456ea
towa0131 <towa.towa0131@gmail.com>
2018-11-02 23:24:18 +0900

    更新

--- index.php
+++ index.php
@@ -9,15 +9,16 @@
 		-->
 		<title>ウサミン星</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="application-name" content="ウサミン星" />
 		<meta name="keywords" content="ウサミン,安部菜々" />
-		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 		<meta name="apple-mobile-web-app-capable" content="yes" />
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 		<meta name="author" content="Abe Nana" />
 		<meta name="theme-color" content="#f08080" />
-		<meta property="og:url" content="http://www.usamin.site" />
-		<meta property="og:image" content="http://www.usamin.site/assets/images/ogp.jpg" />
+		<meta property="og:url" content="https://www.usamin.site" />
+		<meta property="og:image" content="https://www.usamin.site/assets/images/ogp.jpg" />
 		<meta property="og:type" content="website" />
 		<meta property="og:title" content="ウサミン星" />
 		<meta property="og:site_name" content="www.usamin.site" />
@@ -26,13 +27,66 @@
 		<meta name="twitter:card" content="summary_large_image" />
 		<meta name="twitter:title" content="ウサミン星" />
 		<meta name="twitter:description" content="「ナナ、電波がビビッと来ました！」" />
-		<meta name="twitter:image" content="http://www.usamin.site/assets/images/ogp.jpg" />
+		<meta name="twitter:image" content="https://www.usamin.site/assets/images/ogp.jpg" />
 		<link rel="shortcut icon" type="image/png" href="/apple-touch-icon.png" />
 		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 		<link rel="mask-icon" href="/mask-icon.svg" color="#c30234" />
 		<link rel="manifest" href="/assets/manifest.json" />
+		<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="/assets/js/bootstrap.min.js"></script>
 	</head>
-	<body>
-		<p>(*ﾟ∀ﾟ)o彡ﾟﾐﾐﾐﾝ!ﾐﾐﾐﾝ!ｳｰｻﾐﾝ!!</p>
+	<body class="bg-light">
+		<nav class="navbar navbar-expand-lg navbar-light bg-white">
+			<a class="navbar-brand" href="https://www.usamin.site">ウサミン星</a>
+			<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="Navber">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="index.php">ホーム<span class="sr-only"></span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link disabled" href="#">このサイトについて</a>
+						<a class="nav-link disabled" href="#">お問い合わせ</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<div class="container mt-3">
+			<div class="col-sm-8">
+				<div class="alert alert-warning" role="alert">
+					<h4 class="alert-heading">注意</h4>
+					<hr>
+					<p class="mb-0">このページは現在作成中です。
+					一部のコンテンツしか閲覧ができない可能性がございます。</p>
+				</div>
+			</div>
+		<div>
+
+<?php for($i=0;$i<5;$i++){ ?>
+		<div class="container mt-4">
+			<div class="row">
+				<div class="col-sm">
+					<div class="card">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">Sample!</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm">
+					<div class="card">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">Sample!</p>
+						</div>
+					</div>
+				</div>
+			</div>			
+		</div>
+<?php } ?>
 	</body>
 </html>

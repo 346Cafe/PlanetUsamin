82a8dbc06675ccb8e9c60c59052ab7f492e10576
towa0131 <towa.towa0131@gmail.com>
2018-11-02 21:59:03 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8" />
+		<!--
+		 ###################################################################################
+		   『歌って踊れる声優アイドル目指して、ナナはウサミン星からやってきたんですよぉっ。キャハッ☆』
+		 ###################################################################################
+		-->
+		<title>ウサミン星</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="application-name" content="ウサミン星" />
+		<meta name="keywords" content="ウサミン,安部菜々" />
+		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+		<meta name="author" content="Abe Nana" />
+		<meta name="theme-color" content="#f08080" />
+		<meta property="og:url" content="http://www.usamin.site" />
+		<meta property="og:image" content="http://www.usamin.site/assets/images/ogp.jpg" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="ウサミン星" />
+		<meta property="og:site_name" content="www.usamin.site" />
+		<meta property="og:description" content="「ナナ、電波がビビッと来ました！」" />
+		<meta property="og:locale" content="ja_JP" />
+		<meta name="twitter:card" content="summary_large_image" />
+		<meta name="twitter:title" content="ウサミン星" />
+		<meta name="twitter:description" content="「ナナ、電波がビビッと来ました！」" />
+		<meta name="twitter:image" content="http://www.usamin.site/assets/images/ogp.jpg" />
+		<link rel="shortcut icon" type="image/png" href="/apple-touch-icon.png" />
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+		<link rel="mask-icon" href="/mask-icon.svg" color="#c30234" />
+		<link rel="manifest" href="/assets/manifest.json" />
+	</head>
+	<body>
+		<p>(*ﾟ∀ﾟ)o彡ﾟﾐﾐﾐﾝ!ﾐﾐﾐﾝ!ｳｰｻﾐﾝ!!</p>
+	</body>
+</html>
